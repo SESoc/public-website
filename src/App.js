48642750ac4047f69e7bbcfe4710c6027eb0796c
Spencer Dobrik <Dobrik.SpencerA@gmail.com>
2018-10-26 03:27:22 +0000

    Added logo and firebase setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './SE_Logo_inverted_Trimmed.png';
 import './App.css';
 
 class App extends Component {
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Coming soon.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
