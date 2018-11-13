6f628b522be6076d737d6dfd79193d1c739b0dee
Atif Mahmud <atifmahmud101@gmail.com>
2018-11-13 11:28:32 -0500

    Design PlatformSidebarComponent
    References: #10

--- src/containers/Platforms.jsx
+++ src/containers/Platforms.jsx
@@ -80,10 +80,23 @@ function PlatformsList(props) {
   return(listItems);
 }
 
+function PlatformSidebar(props) {
+  return(
+    <div className="sidebar pure-u-1 pure-u-md-1-4">
+      <div className="header">
+        <h1 className="brand-title">SESoc Candidate Platforms</h1>
+        <h1 className="brand-tagline">Winter 2019 Elections</h1>
+      </div>
+    </div>
+  )
+}
+
 class Platforms extends Component {
   render() {
     return (
-      <PlatformsList platforms={platformData}/>
+      <div id="layout" className="pure-g">
+        <PlatformSidebar/>
+      </div>
     );
   }
 }
