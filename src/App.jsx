66b7bd91e3b372944e018cb697d4e887f7028662
root <root@LAPTOP-A7NGJDOD.localdomain>
2019-06-08 15:04:22 -0400

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home.jsx';
 import Platforms from './containers/Platforms.jsx';
 import Events from './containers/Events.jsx';
@@ -46,15 +46,17 @@ class App extends Component {
             </div>
           </div>
         </nav>
+        <Router>
           <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/platforms' component={Platforms}/>
-          <Route exact path='/events' component={Events} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/council' component={Council} />
-          <Route exact path='/sponsors' component={Sponsors} />
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/platforms' component={Platforms}/>
+            <Route exact path='/events' component={Events} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/council' component={Council} />
+            <Route exact path='/sponsors' component={Sponsors} />
           </Switch>
+        </Router>
       </div>
     );
   }
