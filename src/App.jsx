2d9debe17e332b702644999782db05d3fe8d2ed9
root <root@LAPTOP-A7NGJDOD.localdomain>
2019-06-08 12:28:15 -0400

    navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,44 @@ import About from './containers/About.jsx';
 import Contact from './containers/Contact.jsx';
 import Council from './containers/Council.jsx';
 import Sponsors from './containers/Sponsors.jsx';
+import logo from './assets/images/SE_Logo_inverted_Trimmed.png';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <h2>Welcome to the Software Engineering Society!</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/events'} className="nav-link">Events</Link></li>
-            <li><Link to={'/council'} className="nav-link">Council</Link></li>
-            {/*<li><Link to={'/sponsors'} className="nav-link">Sponsors</Link></li>
-             <li><Link to={'/platforms'} className="nav-link">Platforms</Link></li>*/}
-
-          </ul>
-          </nav>
-          <hr />
+        <nav className="navbar navbar-default navbar-fixed-top navbar-expand{-sm|-md|-lg|-xl} navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a href="#" className="navbar-brand">
+              <img src={logo} width="30" className="d-inline-block align-top" alt=""></img>
+              &nbsp; SE Soc
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse primary-header" id="navbarSupportedContent">
+              <div className="primary-nav">
+                <ul className="nav navbar-nav mr-auto">
+                  <li className="n-item active">
+                    <a className="nav-link" href="/">Home</a>
+                  </li>
+                  <li className="n-item">
+                    <a className="nav-link" href="/about" >About Us</a>
+                  </li>
+                  <li className="n-item">
+                    <a className="nav-link" href="/contact">Contact Us</a>
+                  </li>
+                  <li className="n-item">
+                    <a className="nav-link" href="/events">Events</a>
+                  </li>
+                  <li className="n-item">
+                    <a className="nav-link" href="/council">Council</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
           <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/platforms' component={Platforms}/>
@@ -36,7 +55,7 @@ class App extends Component {
           <Route exact path='/council' component={Council} />
           <Route exact path='/sponsors' component={Sponsors} />
           </Switch>
-        </div>
+      </div>
     );
   }
 }
