36310df10cdfce62bbd1f51ad2d95ae1dad58386
Jenny <jgwills@uwaterloo.ca>
2019-06-08 22:02:08 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './containers/Home.jsx';
 import Platforms from './containers/Platforms.jsx';
 import Events from './containers/Events.jsx';
@@ -9,40 +9,28 @@ import Contact from './containers/Contact.jsx';
 import Council from './containers/Council.jsx';
 import Sponsors from './containers/Sponsors.jsx';
 import logo from './assets/images/SE_Logo_inverted_Trimmed.png';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-default navbar-fixed-top navbar-expand{-sm|-md|-lg|-xl} navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a href="/" className="navbar-brand">
-              <img src={logo} width="30" className="d-inline-block align-top" alt=""></img>
-              &nbsp; SE Soc
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse primary-header" id="navbarSupportedContent">
-              <div className="primary-nav">
-                <ul className="nav navbar-nav mr-auto">
-                  <li className="n-item active">
-                    <a className="nav-link" href="/">Home</a>
-                  </li>
-                  <li className="n-item">
-                    <a className="nav-link" href="/about" >About Us</a>
-                  </li>
-                  <li className="n-item">
-                    <a className="nav-link" href="/contact">Contact Us</a>
-                  </li>
-                  <li className="n-item">
-                    <a className="nav-link" href="/events">Events</a>
-                  </li>
-                  <li className="n-item">
-                    <a className="nav-link" href="/council">Council</a>
-                  </li>
-                </ul>
-              </div>
+          <a href="/" className="navbar-brand">
+            <img src={logo} width="30" className="d-inline-block align-top" alt=""></img>
+            &nbsp; SE Soc
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse primary-header" id="navbarSupportedContent">
+            <div className="nav navbar-nav mr-auto">
+              <a activeClassName="active" className="nav-link nav-item" href="/">Home</a>
+              <a activeClassName="active" className="nav-link nav-item" href="/about" >About Us</a>
+              <a activeClassName="active" className="nav-link nav-item" href="/contact">Contact Us</a>
+              <a activeClassName="active" className="nav-link nav-item" href="/events">Events</a>
+              <a activeClassName="active" className="nav-link nav-item" href="/council">Council</a>
             </div>
           </div>
         </nav>
