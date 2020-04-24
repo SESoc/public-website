035b66f0dfd354031f6a7c0f6fbbeb4f20531427
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-04-24 00:23:41 -0700

    Update colors and margin, lint

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,13 @@
-/* 
+/*
 Home page for SE Society
 Last Edited: Tony Zhao 2020-04-22
 */
 
-import React, { Component, Fragment } from 'react';
-import { Button, Container, Jumbotron, Row, Col } from 'react-bootstrap';
-import EventList from '../components/EventList/EventList';
-import NewsList from '../components/NewsList';
+import React, {Component, Fragment} from "react";
+import {Button, Container, Jumbotron, Row, Col} from "react-bootstrap";
+import EventList from "../components/EventList/EventList";
+import NewsList from "../components/NewsList";
+
 const Hero = () => (
   <Jumbotron>
     <Container>
@@ -24,41 +25,51 @@ const Hero = () => (
             our desire to create events to support our program socially and
             professionally]
           </p>
-          <a href="/events">something about our latest move --></a>
+          <a href="/events" style={{color:"var(--color-accent-eng)"}}>something about our latest move --&gt</a>
         </Col>
       </Row>
     </Container>
   </Jumbotron>
 );
 
+const contentSectionStyle = {
+  marginBottom: "50px",
+};
 const Content = () => (
   <Container>
     <Row>
       <Col sm>
-        <NewsList
-          numEntries={3}
-          articles={[
-            {
-              title: 'Upcoming Elections',
-              description: 'Checkout the candidate profiles',
-            },
-            {
-              title: 'SESX S01E01',
-              description: 'Running a Startup as an SE Student',
-            },
-            {
-              title: 'Ibrahim: What I wish I knew in 1A',
-              description: 'Intro to the blog',
-            },
-          ]}
-        />
-        <Button variant="secondary" href="/news">
+        <section style={contentSectionStyle}>
+          <NewsList
+            numEntries={3}
+            articles={[
+              {
+                title: "Upcoming Elections",
+                description: "Checkout the candidate profiles",
+              },
+              {
+                title: "SESX S01E01",
+                description: "Running a Startup as an SE Student",
+              },
+              {
+                title: "Ibrahim: What I wish I knew in 1A",
+                description: "Intro to the blog",
+              },
+            ]}
+          />
+          <Button variant="secondary" href="/news">
           See Archive
-        </Button>
+          </Button>
+        </section>
       </Col>
+
       <Col sm>
-        <EventList />
-        <Button variant="secondary" href="/events">See All</Button>
+        <section style={contentSectionStyle}>
+          <EventList />
+          <Button variant="secondary" href="/events">
+          See All
+          </Button>
+        </section>
       </Col>
     </Row>
   </Container>
