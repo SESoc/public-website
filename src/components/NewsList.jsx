46f0ddc6910b3e278f1757c928b1885fd3fd2bfd
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-04-24 00:22:51 -0700

    Remove <br> use

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const NewsContent = (props) => {
   //TODO: conditionally render color
-  const numEntries = props.numEntries ? props.numEntries : props.articles.length;
-    return props.articles.splice(0, numEntries).map((article) => (
-      <tr>
-        <th>
-          <p> {article.title} <br></br>
-            <span> {article.description} </span>
-          </p>
-        </th>
-      </tr>
-    ));
+  const numEntries = props.numEntries
+    ? props.numEntries
+    : props.articles.length;
+  return props.articles.splice(0, numEntries).map((article) => (
+    <tr>
+      <th>
+        <p style={{marginBottom: 0}}> {article.title} </p>
+        <p>
+          <span> {article.description} </span>
+        </p>
+      </th>
+    </tr>
+  ));
 };
 
 const NewsList = (props) => (
