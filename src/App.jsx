21ef5ca1f63cec0eb8ff165082b2bc7bd6a70af0
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-04-26 01:08:25 -0700

    Convert to react-bootstrap component

--- src/App.jsx
+++ src/App.jsx
@@ -8,76 +8,14 @@ import About from "./containers/About.jsx";
 import News from "./containers/News.jsx";
 import Council from "./containers/Council.jsx";
 import Sponsors from "./containers/Sponsors.jsx";
-import logo from "./assets/images/SE_Logo_inverted_Trimmed.png";
+import NavigationBar from "./components/NavigationBar.jsx";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-default navbar-fixed-top navbar-expand{-sm|-md|-lg|-xl} navbar-expand-lg navbar-light bg-light">
-          <a href="/" className="navbar-brand">
-            <img
-              src={logo}
-              width="30"
-              className="d-inline-block align-top"
-              alt=""
-            ></img>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse primary-header"
-            id="navbarSupportedContent"
-          >
-            <div className="nav navbar-nav mr-auto">
-              <a
-                activeClassName="active"
-                className="nav-link nav-item"
-                href="/"
-              >
-                home
-              </a>
-              <a
-                activeClassName="active"
-                className="nav-link nav-item"
-                href="/events"
-              >
-                events
-              </a>
-              <a
-                activeClassName="active"
-                className="nav-link nav-item"
-                href="/news"
-              >
-                news
-              </a>
-              <a
-                activeClassName="active"
-                className="nav-link nav-item"
-                href="/council"
-              >
-                council
-              </a>
-              <a
-                activeClassName="active"
-                className="nav-link nav-item"
-                href="/about"
-              >
-                about
-              </a>
-            </div>
-          </div>
-        </nav>
-        <Router>
+      <Router>
+        <div>
+          <NavigationBar />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/events" component={Events} />
@@ -87,8 +25,8 @@ class App extends Component {
             <Route exact path="/platforms" component={Platforms} />
             <Route exact path="/sponsors" component={Sponsors} />
           </Switch>
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import logo from "../assets/images/SE_Logo_inverted_Trimmed.png";
+import {Container , Nav, Navbar} from "react-bootstrap";
+import {Link, NavLink} from "react-router-dom";
+
+const NavigationBar = () => (
+  <Navbar bg="light" expand="lg" collapseOnSelect >
+    <Container>
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src={logo}
+          width="30"
+          className="d-inline-block align-top"
+          alt="SE Society"
+        />
+      </Navbar.Brand>
+
+      <Navbar.Toggle
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      />
+
+      <Navbar.Collapse>
+        <Nav className="ml-auto">
+          <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            exact
+            to="/">
+          home
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            exact
+            to="/events"
+          >
+          events
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            exact
+            to="/news">
+          news
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            exact
+            to="/council"
+          >
+          council
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            className="nav-link nav-item"
+            to="/about">
+          about
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default NavigationBar;
