d711db12a6308de8b8ebbc382b2379aa89ceab52
brzhuang <robbie.zhuang@gmail.com>
2020-05-05 20:03:48 -0400

    Update logic

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -8,9 +8,10 @@ const Event = props => (
     <p className="mb-0">{props.event.dateLabel}</p>
     <p>
       <span className="accent">
-        {" "}
         {props.event.title}
-        {props.event.description === "" ? ": " : ""}{" "}
+        {!props.event.description || props.event.description.length === 0
+          ? ""
+          : ": "}
       </span>
       {props.event.description}
     </p>
