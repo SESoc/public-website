ff1188fed7859104ddfc0e9e241a367331e0c00b
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-05-20 22:39:33 -0700

    Navigation bar color based on route

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,78 +1,78 @@
 import React from "react";
-import url from 'url';
+import PropTypes from "prop-types";
+import url from "url";
 import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link, NavLink} from "react-router-dom";
-import "./styles/NavigationBar.css";
+import {Link, NavLink, useLocation} from "react-router-dom";
+import "./styles/NavigationBar.scss";
 
 const logo = url.resolve(process.env.PUBLIC_URL, "/icons/se-logo-trimmed.png");
 
-const NavigationBar = () => (
-  <Navbar bg="transparent" expand="sm" collapseOnSelect >
-    <Container>
-      <Navbar.Brand as={Link} to="/">
-        <img
-          src={logo}
-          width="30"
-          className="d-inline-block align-top"
-          alt="SE Society"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      />
+const NavigationBar = (props) => {
+  let location = useLocation();
+  let color = "";
+  switch (location.pathname) {
+  case "/":
+    color = "#F0EDF1";
+    break;
+  default:
+    color = "transparent";
+    break;
+  }
 
-      <Navbar.Collapse>
-        <Nav className="ml-auto">
-          <Nav.Link
-            as={NavLink}
-            activeClassName="active"
-            className="mr-4"
-            exact
-            to="/">
-          home
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            activeClassName="active"
-            className="mr-4"
-            exact
-            to="/events"
-          >
-          events
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            activeClassName="active"
-            className="mr-4"
-            exact
-            to="/news">
-          news
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            activeClassName="active"
-            className="mr-4"
-            exact
-            to="/council"
-          >
-          council
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            activeClassName="active"
-            className="mr-4"
-            exact
-            to="/about">
-            about
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+  return (
+    <Navbar className="mx-3 mx-sm-auto" expand="sm" collapseOnSelect style={{backgroundColor:color}}>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            width="30"
+            className="d-inline-block align-top"
+            alt="SE Society"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        />
+        <Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Nav.Link
+              as={NavLink}
+              activeClassName="active"
+              className="mr-4"
+              exact
+              to="/">
+          Home
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              activeClassName="active"
+              className="mr-4"
+              exact
+              to="/about">
+          About
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              activeClassName="active"
+              className="mr-4"
+              exact
+              to="/events"
+            >
+          Events
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+// Props from withRouter HOC
+NavigationBar.propTypes = {
+};
 
 export default NavigationBar;
