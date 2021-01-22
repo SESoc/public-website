12d4c6140eea6fe912ba695ddc19c84aca8ba83c
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2021-01-22 14:02:46 -0500

    add title and alt to iframe

--- src/containers/Wise.jsx
+++ src/containers/Wise.jsx
@@ -323,7 +323,13 @@ const Videos = () => (
   <>
     <h2 className="pt-5 pb-2">Videos</h2>
     <h4>WiSE Upper Year Panel: Fall 2020</h4>
-    <iframe src="https://drive.google.com/file/d/1ouxt001mRjDq3p1StSJ57d7wXxxBpMdC/preview" width="1000" height="670"></iframe>
+    <iframe
+      title="WiSE Upper Year Panel: Fall 2020"
+      alt="WiSE Upper Year Panel: Fall 2020"
+      src="https://drive.google.com/file/d/1ouxt001mRjDq3p1StSJ57d7wXxxBpMdC/preview"
+      width="1000"
+      height="670"
+    />
   </>
 );
 
